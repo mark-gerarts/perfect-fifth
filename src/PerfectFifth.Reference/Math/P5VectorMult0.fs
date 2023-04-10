6b492e1eec7da1b0f3c582f362d6fcf1a61ecbe7
Mark Gerarts <mark.gerarts@gmail.com>
2023-04-10 09:42:40 +0200

    Prepare Math reference sketches

---
+++ src/PerfectFifth.Reference/Math/P5VectorMult0.fs
@@ -0,0 +1,13 @@
+module P5Reference.Math.P5VectorMult0
+
+open P5.Core
+open P5.Color
+open P5.Environment
+open P5.Shape
+
+let draw p5 =
+    strokeWeight p5 4
+    stroke p5 (Grayscale 51)
+    square p5 20 20 60
+
+let run node = display node draw
